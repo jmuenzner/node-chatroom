34c4a45edff0dc1d4cb380668e40b6e53cd6cedd
Jay Muenzner <jmuenzner@gmail.com>
2012-01-27 09:59:28 +1100

    Change instructions.

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ var colours = ['red', 'blue', 'green', 'blueviolet', 'burlywood', 'cadetblue', '
 
 // The user list gets sorted twice - when a user joins and leaves
 function sortUserList(users) {
-	var sorted = [];
-	for ( user in users )
-		sorted.push(user);
+    var sorted = [];
+    for ( user in users )
+        sorted.push(user);
 	
-	return sorted.sort();
+    return sorted.sort();
 }
 
 // As connections come in deal with them here.
@@ -36,26 +36,26 @@ io.sockets.on('connection', function(client) {
     // When the user submits the nickname it gets processed here
     client.on('register', function(username){
 
-		// This is definitely not the most in depth validation, but I thought 
-		// instead of writing many lines to handle all possible cases I use
-		// some more general cases.
-		if ( username == '' || username == undefined || users[username] != undefined || username.length > 10 ) {
-			client.emit('register', 'Username is not valid. Please try a different one.');
-		} else {
+        // This is definitely not the most in depth validation, but I thought 
+        // instead of writing many lines to handle all possible cases I use
+        // some more general cases.
+        if ( username == '' || username == undefined || users[username] != undefined || username.length > 10 ) {
+            client.emit('register', 'Username is not valid. Please try a different one.');
+        } else {
 
 	        // Keep the username for this session
 	        client.username = username;	
 	
-			// Also add the user and the assigned colour in a global list for broadcasting to everyone
-			users[username] = colours.shift();
+            // Also add the user and the assigned colour in a global list for broadcasting to everyone
+            users[username] = colours.shift();
 			
-			// Send out the updated user list
-			// Sort it for better readability. Do this here so that each client does not have to do it.
-			io.sockets.emit('userlist', users, sortUserList(users));
+            // Send out the updated user list
+            // Sort it for better readability. Do this here so that each client does not have to do it.
+            io.sockets.emit('userlist', users, sortUserList(users));
 			
-			// Tell everyone about the new user
+            // Tell everyone about the new user
 	        client.broadcast.emit('useractivity', username, 'has joined this room.');
-		}
+        }
     });
 
     // When a new message comes in from a user then send it to everyone.
@@ -65,21 +65,21 @@ io.sockets.on('connection', function(client) {
         io.sockets.emit('updatechat', client.username, users[client.username], message);
     });
 
-	// A couple of things need to happen when a user disconnects
-	client.on('disconnect', function() { 
+    // A couple of things need to happen when a user disconnects
+    client.on('disconnect', function() { 
 		
-		// Free up the user's colour so it can be reused.
-		colours.push(users[client.username]);
+        // Free up the user's colour so it can be reused.
+        colours.push(users[client.username]);
 		
-		// Clear the user from the global list
-		delete users[client.username];
+        // Clear the user from the global list
+        delete users[client.username];
 		
-		// Update the userlist for all clients. The list needs to be re-sorted.
-		// Actually, maybe it doesn't, but there could be a race condition if new users join
-		// at this point!?
-		io.sockets.emit('userlist', users, sortUserList(users));
+        // Update the userlist for all clients. The list needs to be re-sorted.
+        // Actually, maybe it doesn't, but there could be a race condition if new users join
+        // at this point!?
+        io.sockets.emit('userlist', users, sortUserList(users));
 		
-		// Tell everyone that this user is gone.
-		io.sockets.emit('useractivity', client.username, ' has left this room');
-	});
+        // Tell everyone that this user is gone.
+        io.sockets.emit('useractivity', client.username, ' has left this room');
+    });
 });
